f4d9b325e51865a4af46c92afc3f3f4b02c49aa6
Trevor Page <trevg60@gmail.com>
2012-11-30 19:51:48 +0000

    Updated example for compatibility with development of library.

--- TPSVG_Example1/src/com/trevorpage/tpsvg_example1/TPSVG_Example1Activity.java
+++ TPSVG_Example1/src/com/trevorpage/tpsvg_example1/TPSVG_Example1Activity.java
@@ -1,11 +1,7 @@
 package com.trevorpage.tpsvg_example1;
 
-import com.trevorpage.tpsvg.SVGFlyweightFactory;
+import com.trevorpage.tpsvg.SVGParserRenderer;
 import com.trevorpage.tpsvg.SVGView;
-import com.trevorpage.tpsvg.Tpsvg;
-import com.trevorpage.tpsvg.Tpsvg.SvgStyle;
-import com.trevorpage.tpsvg.Tpsvg.Textstring;
-
 import android.app.Activity;
 import android.graphics.Matrix;
 import android.graphics.Paint;
@@ -13,34 +9,44 @@ import android.graphics.Path;
 import android.graphics.RectF;
 import android.os.Bundle;
 
-public class TPSVG_Example1Activity extends Activity implements com.trevorpage.tpsvg.ItpsvgAnim {
-    /** Called when the activity is first created. */
+public class TPSVG_Example1Activity extends Activity implements com.trevorpage.tpsvg.ITpsvgController {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main);
         SVGView svgView = (SVGView)this.findViewById(R.id.svgImage);
-        svgView.setImageResource(R.raw.anime, this);
+        SVGParserRenderer image = new SVGParserRenderer(this, R.raw.anime);
+        svgView.setSVGRenderer(image, null);
         svgView.setBackgroundColor(0xff123456);
     }
 
     
     
-	public boolean animElement(String id, int iteration, Matrix matrix,
-			Paint sroke, Paint fill) {
-		// TODO Auto-generated method stub
+	public boolean animElement(String id, int iteration, Matrix matrix, Paint sroke, Paint fill) {
+		return false;
+	}
+
+	public boolean arcParams(String id, Path path, float startAngle, float sweepAngle, RectF bounds) {
 		return false;
 	}
 
 	public boolean animTextElement(String id, int iteration, Matrix matrix,
-			SvgStyle style, Textstring text, float x, float y) {
-		// TODO Auto-generated method stub
+			com.trevorpage.tpsvg.SVGParserRenderer.SvgStyle style,
+			com.trevorpage.tpsvg.SVGParserRenderer.Textstring text, float x,
+			float y) {
 		return false;
 	}
 
-	public boolean arcParams(String id, Path path, float startAngle,
-			float sweepAngle, RectF bounds) {
-		// TODO Auto-generated method stub
-		return false;
+	public void setSourceDocumentWidth(int width) {
+		
+	}
+
+	public void setSourceDocumentHeight(int height) {
+		
+	}
+
+	public void onSVGPrivateData(String key, String value) {
+
 	}
 }
